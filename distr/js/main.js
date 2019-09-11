1a87d63ff7e285bebf71f432c0cd725ee50fa6fa
Francesca Serafini <serafini.francesca2@gmail.com>
2019-09-11 21:30:49 +0200

    clock

--- distr/js/main.js
+++ distr/js/main.js
@@ -1,2 +1,19 @@
+$(document).ready(function () {
+    var SECONDS = 1000;
+    var MINUTES = 60 * SECONDS;
+    
+    setInterval(updateClock, SECONDS);
+
+});
 
 
+function updateClock() {
+
+    var today = new Date();
+    var time = today.getHours() + ':' + today.getMinutes();
+
+    $("#clock").html(time);
+
+    console.log(time);
+
+}
